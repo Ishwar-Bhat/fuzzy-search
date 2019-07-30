dff929cdc5f2fcc4f16048d8ba8f377a2868a9da
Ishwar-Bhat <ishwarbhat01@gmail.com>
2019-07-30 09:49:18 +0530

    Created models and ran migrations'

--- FuzzySearch/add_words/models.py
+++ FuzzySearch/add_words/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class WordsList(models.Model):
+    word = models.CharField(primary_key=True, max_length=100, blank=False)
+    frequency = models.IntegerField(null=False)
