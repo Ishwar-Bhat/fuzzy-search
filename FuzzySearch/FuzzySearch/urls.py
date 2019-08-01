9b917168ef6ebb50d85f4682386eeed91cec8fc5
Ishwar-Bhat <ishwarbhat01@gmail.com>
2019-08-01 14:37:07 +0530

    JS functions

--- FuzzySearch/FuzzySearch/urls.py
+++ FuzzySearch/FuzzySearch/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 import add_words
 
 urlpatterns = [
+    path('', include('search.urls')),
     path('admin/', admin.site.urls),
     path('add/', include('add_words.urls')),
-    path('', include('search.urls'))
+    path('search', include('api.urls'))
 ]
--- FuzzySearch/api/views.py
+++ FuzzySearch/api/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+import json
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse
+from add_words.models import WordsList
 
-# Create your views here.
+
+def search_api(req):
+    response = []
+    input_val = req.GET['word']
+    try:
+        exact_match = WordsList.objects.get(word=input_val)
+        response.append(exact_match.word)
+    except ObjectDoesNotExist:
+        pass
+    # words_list = WordsList.objects.filter(word__contains=input_val)[:25]
+    words_list = WordsList.objects.filter(word__startswith=input_val)
+    for word in words_list:
+        response.append(word.word)
+        # response = []
+        # words_
+        # input_val = req.GET['word']
+        # try:
+        #     exact_match = WordsList.objects.get(word=input_val)
+        # except ObjectDoesNotExist:
+        #     exact_match = None
+        # # words_list = WordsList.objects.filter(word__contains=input_val)[:25]
+        # words_list = WordsList.objects.filter(word__startswith=input_val).order_by('-frequency')
+        # for word in words_list:
+        #     words_startwith.append(word.word)
+        # words_list = WordsList.objects.filter(word__contains=input_val).exclude(words__in=words_startwith).order_by(
+        #     '-frequency')
+        # for word in words_list:
+        #     words_contains.append(word.word)
+        # return HttpResponse(json.dumps(response), content_type='application/json')
+    return HttpResponse(json.dumps(response[:25]), content_type='application/json')
